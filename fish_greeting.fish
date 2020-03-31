0a3264d065c2de732b89e15f8e9dcf8f0248f697
Marvin Eversley Silva <meverss@outlook.com>
2020-03-31 00:09:37 -0400

    Update Tue Mar 31 00:05:04 CDT 2020

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,31 +1,12 @@
-###############################################################################
-#
-# prompt theme name:
-#   budspencer
-#
-# description:
-#   a sophisticated airline/powerline theme
-#
-# author:
-#   joseph tannhuber <sepp.tannhuber@yahoo.de>
-#
-# sections:
-#   -> Welcome message
-#
-###############################################################################
-
-####################
-# => Welcome message
-####################
 function fish_greeting -d 'Show greeting in login shell.'
-  if not set -q budspencer_nogreeting
+  if not set -q barracuda_nogreeting
     if begin
       not set -q -x LOGIN
       and not set -q -x RANGER_LEVEL
       and not set -q -x VIM
       end
-      echo This is (set_color -b $budspencer_colors[2] $budspencer_colors[10])budspencer(set_color normal) theme for fish, a theme for nerds.
-      echo Type (set_color -b $budspencer_colors[2] $budspencer_colors[6])»budspencer_help«(set_color normal) in order to see how you can speed up your workflow.
+      echo This is (set_color -b $barracuda_colors[2] $barracuda_colors[10])barracuda(set_color normal) theme for fish, a theme for nerds.
+      echo Type (set_color -b $barracuda_colors[2] $barracuda_colors[6])»barracuda_help«(set_color normal) in order to see how you can speed up your workflow.
       end
   end
 end
