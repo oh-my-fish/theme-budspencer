80b82208e10897fa3eca373740c1920e5e53c6c2
nicolas <ngmoviedo@gmx.com>
2021-01-26 12:14:03 +0100

    Greeting now shows even if LOGIN is set

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -20,8 +20,7 @@
 function fish_greeting -d 'Show greeting in login shell.'
   if not set -q budspencer_nogreeting
     if begin
-      not set -q -x LOGIN
-      and not set -q -x RANGER_LEVEL
+      not set -q -x RANGER_LEVEL
       and not set -q -x VIM
       end
       echo This is (set_color -b $budspencer_colors[2] $budspencer_colors[10])budspencer(set_color normal) theme for fish, a theme for nerds.
